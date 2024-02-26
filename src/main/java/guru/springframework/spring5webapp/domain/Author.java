e17175ba49f7d0e307785f544dd1ff69680dcacf
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-02-26 17:35:16 -0500

    Fixes circular reference in tostring methods

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -57,7 +57,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
